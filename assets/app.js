90acce467850191a4bb3f3aa66c3f198aa6e3480
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-22 20:09:27 -0700

    test redirect google log in

--- assets/app.js
+++ assets/app.js
@@ -42,7 +42,7 @@ var firebaseConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<train-scheduler-a0a02.firebaseapp.com>',
+    signInSuccessUrl: '<>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
