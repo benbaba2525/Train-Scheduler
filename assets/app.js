3dee33cfcbbe62a7dff81498ff56b158e1628709
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-22 20:23:51 -0700

    Use code signInSuccessUrl:<url-to-redirect-to-on-success>

--- assets/app.js
+++ assets/app.js
@@ -42,7 +42,7 @@ var firebaseConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'scheduleBroad.html',
+    signInSuccessUrl:"<url-to-redirect-to-on-success>",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
