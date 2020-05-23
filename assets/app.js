6e542666af972ff8330a64542cc63bba71f96d95
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-22 20:43:00 -0700

    Change back to schedule.html

--- assets/app.js
+++ assets/app.js
@@ -42,7 +42,7 @@ var firebaseConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl:"<url-to-redirect-to-on-success>",
+    signInSuccessUrl:"scheduleBroad.html",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
